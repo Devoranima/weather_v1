28ea8e2a7f8c90b09113f551686b46306f62ee36
Devoranima <ali3v5t1limAw@gmail.com>
2023-03-30 21:19:51 +0300

    md correct

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import Weather  from "./components/Weather";
+import React, {useState, useEffect} from 'react';
+import WeatherServices from "./services/Weather-api";
+import { useFetching } from "./hooks/useFetching";
+import { useWeather } from "./hooks/useWeather";
+
+import {Loader, Dimmer, Segment} from "semantic-ui-react"
 
 function App() {
+  const [weatherData, setWeatherData] = useState({});
+
+  const [city, setCity] = useState('Moscov');
+
+  useEffect(()=>{
+    navigator.geolocation.getCurrentPosition((position)=>{
+        const newCity = position.coords.latitude+','+position.coords.longitude;
+        setCity(newCity);
+        fetchWeather(newCity);
+    });
+  }, [])
+
+  useEffect(()=>{
+    fetchWeather(city)
+  }, [city])
+
+  
+  const [fetchWeather, isWeatherLoading, weatherError] = useFetching(async(city) =>{
+    const response = await WeatherServices.getAll(city);
+    setWeatherData(response.data);
+  })
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className='App'>
+          {weatherError && 
+              <h1>An error occured: {weatherError.toString()}</h1>
+          }
+
+          {isWeatherLoading?
+          <Loader active inverted size="massive"/>
+          :
+          <Weather fetchweather={fetchWeather} {...weatherData}></Weather>
+          }
+          
+          
+      </div>
+  )
 }
 
 export default App;
